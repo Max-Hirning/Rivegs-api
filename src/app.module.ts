620788a4639637a5e6779b91ee03f69bc12cc635
Max-Hirning <maxhirning25@gmail.com>
2024-04-01 21:31:22 +0300

    Connected modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
+import {JwtModule} from '@nestjs/jwt';
 import {Module} from '@nestjs/common';
 import {AppService} from './app.service';
+import {ConfigModule} from '@nestjs/config';
 import {AppController} from './app.controller';
+import {MongooseModule} from '@nestjs/mongoose';
+import {MailerModule} from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({envFilePath: '.env', isGlobal: true}),
+    MailerModule.forRoot({
+      transport: {
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.EMAIL_PASS,
+        },
+        port: 465, 
+        secure: true,
+        ignoreTLS: true,
+        host: 'smtp.gmail.com',
+      },
+      defaults: {
+        sender: process.env.EMAIL,
+        replyTo: process.env.EMAIL,
+        from: `"No Reply" ${process.env.EMAIL}`,
+      },
+    }),
+    MongooseModule.forRoot(process.env.DB_URL, {dbName: 'Rivegs'}),
+    JwtModule.register({signOptions: {expiresIn: process.env.JWT_TOKEN_EXPIRES_IN}, secret: process.env.SECRET_KEY}),
+  ],
   providers: [AppService],
   controllers: [AppController],
 })
